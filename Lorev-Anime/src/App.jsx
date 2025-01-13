e5250cb9c08d27572ad3e3afd96b6b6972868d33
6godpro <erickson5115@gmail.com>
2025-01-13 15:00:56 +0100

    hero component created

---
+++ Lorev-Anime/src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero/Hero";
+import NavBar from "./components/Navbar/NavBar";
+import Products from "./components/Products/Products";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
