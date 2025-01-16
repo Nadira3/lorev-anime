19fafbc83dd8fc10e13bcfb3e287980bc5d5e511
6godpro <erickson5115@gmail.com>
2025-01-16 15:21:01 +0100

    Implemented dark navbar styling

--- Lorev-Anime/src/components/Navbar/NavBar.jsx
+++ Lorev-Anime/src/components/Navbar/NavBar.jsx
@@ -1,15 +1,29 @@
 import "./navbar.css";
 import logo from "../../assets/logo.svg";
 import meniIcon from "../../assets/menu-icon.png";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const NavBar = () => {
-  const [menuInvisible, setMenuVisible] = useState(false);
+  const [darkNav, setDarkNav] = useState(false);
   const toggleMenu = () => {
     menuInvisible === false ? setMenuVisible(true) : setMenuVisible(false);
   };
+  
+  const [menuInvisible, setMenuVisible] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 700 ? setDarkNav(true) : setDarkNav(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="container">
+    <nav className={darkNav ? "container dark-nav" : "container"}>
       <img src={logo} alt="logo" className="logo" />
       <ul className={!menuInvisible ? "hide-menu" : ""}>
         <li>Events</li>
