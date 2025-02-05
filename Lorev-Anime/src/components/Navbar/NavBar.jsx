d7c93ad9d5491cc3514144859cc0e5d2f7cd5090
6godpro <erickson5115@gmail.com>
2025-02-05 20:33:38 +0100

    Implemented nav links scroll to corresponding section

--- Lorev-Anime/src/components/Navbar/NavBar.jsx
+++ Lorev-Anime/src/components/Navbar/NavBar.jsx
@@ -2,6 +2,7 @@ import "./navbar.css";
 import logo from "../../assets/logo.svg";
 import meniIcon from "../../assets/menu-icon.png";
 import { useState, useEffect } from "react";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
   const [darkNav, setDarkNav] = useState(false);
@@ -26,10 +27,24 @@ const NavBar = () => {
     <nav className={darkNav ? "container dark-nav" : "container"}>
       <img src={logo} alt="logo" className="logo" />
       <ul className={!menuInvisible ? "hide-menu" : ""}>
-        <li>Events</li>
-        <li>Products</li>
-        <li>Documentary</li>
-        <li>Motivational</li>
+        <li>
+          <Link to="products" smooth="true" duration={500} offset={-260}>
+            Products
+          </Link>
+        </li>
+        <li>
+          <Link to="events" smooth="true" duration={500} offset={-260}>
+            Events
+          </Link>
+        </li>
+        <li>
+          <Link to="documentaries" smooth="true" duration={500} offset={-260}>
+            Documentary
+          </Link>
+        </li>
+        <li><Link to="motivationals" smooth="true" duration={500} offset={-260}>
+            Motivational
+          </Link></li>
       </ul>
       <img
         src={meniIcon}
